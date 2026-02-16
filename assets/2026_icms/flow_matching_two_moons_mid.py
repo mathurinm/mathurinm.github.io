0a69f4bdc0639dc2f9ee204f6961ee040ef58952
mathurinm <mathurinm@users.noreply.github.com>
2026-02-16 11:39:58 +0000

    deploy: f82329600a4724f3c193e8c311a5d9dc2fda2a1c

--- assets/2026_icms/flow_matching_two_moons_mid.py
+++ assets/2026_icms/flow_matching_two_moons_mid.py
@@ -24,54 +24,55 @@ class Flow(nn.Module):
         """Euler ODE solver"""
         t_start = t_start.view(1, 1).expand(x_t.shape[0], 1)
         delta = t_end - t_start
-        return x_t + delta * self(x_t + self(x_t, t_start) * delta/2,
-                                  t_start + delta/2)
+        return x_t + delta * self(x_t, t_start)
 
 
-# %%
+# %% network training
 flow = Flow()
 optimizer = torch.optim.Adam(flow.parameters(), 1e-2)
-loss_fn = nn.MSELoss()
+loss_fn = nn.MSELoss()  # squared L2 loss for network training
 
-for it in range(1_000):
-    print(it)
+for it in range(10_000):
+    if it % 500 == 0:
+        print(it)
     x_1 = Tensor(make_moons(256, noise=0.05)[0])
     x_0 = torch.randn_like(x_1)
-    t = torch.rand(256, 1)
-    # t = TODO (must be of shape (256, 1))
-
-    # x_t = TODO
+    t = torch.rand(len(x_1), 1)
     x_t = (1 - t) * x_0 + t * x_1
-    u_true = x_1 - x_0
-    # u_true = TODO
     optimizer.zero_grad()
-    # loss = TODO
-    loss = loss_fn(u_true, flow(x_t, t))
+    ut_xt = flow(x_t, t)
+    loss = loss_fn(ut_xt, x_1 - x_0)
     loss.backward()
     optimizer.step()
 
-# %%
+# %% plots
 plt.close('all')
-x = torch.randn(300, 2)
-n_steps = 8
-time_steps = torch.linspace(0, 1, n_steps+1)
-fig, ax = plt.subplots(1, n_steps + 1, sharex=True, sharey=True, figsize=(15, 3))
-ax[0].scatter(*x.detach().T, s=10, alpha=0.5)
-ax[0].set_xlim(-3, 3)
-ax[0].set_ylim(-3, 3)
-ax[0].set_title(f't = 0.00', fontsize=18)
 
+tab10 = plt.colormaps["tab10"]
+fig, axarr = plt.subplots(1, 2, sharex=True, sharey=True)
+x1 = make_moons(256, noise=0.05)[0]
+axarr[0].scatter(*x1.T, color=tab10.colors[1])
+axarr[0].set_title("True data")
+axarr[1].scatter(*x.T.detach(), color=tab10.colors[1])
+axarr[1].set_title("Generated data")
+plt.show(block=False)
+plt.savefig("2moons_goal.pdf")
 
-for i in range(n_steps):
-    x = flow.step(x, time_steps[i], time_steps[i+1])
-    ax[i + 1].scatter(*x.detach().T, s=10, alpha=0.5)
-    ax[i + 1].set_title(f't = {time_steps[i + 1]:.2f}', fontsize=18)
+# %%
+n_plots = 8
+fig, ax = plt.subplots(1, n_plots, sharex=True, sharey=True, figsize=(15, 3))
+
+for idx, i in enumerate(np.linspace(0, n_steps - 1, n_plots, endpoint=True)):
+    x = xt[int(i)]
+    ax[idx].scatter(*x.detach().T, s=10, alpha=0.5)
+    ax[idx].set_title(f't = {time_steps[int(i) + 1]:.2f}', fontsize=18)
 
 for a in ax:
     a.set_aspect("equal")
+    a.set_xlim(-3, 3)
+    a.set_ylim(-3, 3)
+
 plt.tight_layout()
 plt.show(block=False)
 
 plt.savefig("lab.pdf")
-
-# %%
